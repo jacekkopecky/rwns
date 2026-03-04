a27cda685ec6557f5ecb1ae4926bb91cb9971533
Jacek Kopecky <jacek@jacek.cz>
2026-03-04 20:10:36 +0000

    add fullscreen

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('here');
+document.body.addEventListener('keyup', handleTopLevelSpaceKey);
+document.querySelector('#startBtn')?.addEventListener('click', goFullscreen);
+document.querySelector('#exitBtn')?.addEventListener('click', exitFullscreen);
+
+function goFullscreen() {
+  document.querySelector('main')?.requestFullscreen();
+}
+
+function exitFullscreen() {
+  document.exitFullscreen();
+}
+
+function handleTopLevelSpaceKey(e: KeyboardEvent): void {
+  if (e.key === ' ' && !document.fullscreenElement) {
+    goFullscreen();
+  }
+}
