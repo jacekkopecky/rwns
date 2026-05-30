820805a0360644c3daf51d62e9778075139f6b5e
Jacek Kopecky <jacek@jacek.cz>
2026-05-30 14:19:45 +0100

    :shirt: make eslint stricter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,17 +4,33 @@ import tseslint from 'typescript-eslint';
 
 export default defineConfig(
   js.configs.recommended,
-  tseslint.configs.recommended,
-  // tseslint.configs.strict,
+  tseslint.configs.strict,
   // tseslint.configs.recommendedTypeChecked,
   // tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
-  globalIgnores(['./*.js', './*.cjs', './*.mjs', 'docs', 'dist', 'dev-dist', 'node_modules']),
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
       },
+    },
+  },
+  {
+    ignores: [
+      './*.js',
+      './*.cjs',
+      './*.mjs',
+      'docs',
+      'dist',
+      'dev-dist',
+      'node_modules',
+      './src/testing',
+    ],
+  },
+  {
+    rules: {
+      // often we know well enough that something is there
+      '@typescript-eslint/no-non-null-assertion': 'off',
     },
   },
 );
