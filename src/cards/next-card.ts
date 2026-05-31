468939ce3e7dd786d7d33923adb54ef61946d151
Jacek Kopecky <jacek@jacek.cz>
2026-05-31 18:41:21 +0100

    give cards by rarity

--- src/cards/next-card.ts
+++ src/cards/next-card.ts
@@ -1,11 +1,11 @@
 import { CARDS, type CardType, type ReadonlyState } from '#types';
-import { pickWeightedItem } from '#utils';
+import { pickMapWeightedItem, randomItem } from '#utils';
 
 import { cardDefinitions, type Rarity } from './types';
 
 const rarityFactors: Readonly<Record<Rarity, number>> = {
-  common: 27,
-  rare: 9,
+  common: 96,
+  rare: 12,
   epic: 3,
   legendary: 1,
 };
@@ -14,12 +14,12 @@ export function selectNextRandomCard(state: ReadonlyState): CardType | undefined
   const availableTypes = CARDS.filter((type) => isTypeAvailable(state, type));
   if (availableTypes.length <= 1) return availableTypes[0];
 
-  const weightedTypes = availableTypes.map((type) => ({
-    value: type,
-    weight: rarityFactors[cardDefinitions[type].rarity],
-  }));
+  const availableRarities = [...new Set(availableTypes.map((t) => cardDefinitions[t].rarity))];
+  const selectedRarity = pickMapWeightedItem(availableRarities, rarityFactors, Math.random);
 
-  return pickWeightedItem(weightedTypes, Math.random);
+  const typesAtRarity = availableTypes.filter((t) => cardDefinitions[t].rarity === selectedRarity);
+
+  return randomItem(typesAtRarity, Math.random);
 }
 
 function isTypeAvailable(state: ReadonlyState, type: CardType) {
