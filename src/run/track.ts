28b92e986870aebab6af5e867864ea946ae5e819
Jacek Kopecky <jacek@jacek.cz>
2026-06-08 13:22:41 +0100

    continue fixing track clearing problems
    the track now seemed to be generated completely behind us

--- src/run/track.ts
+++ src/run/track.ts
@@ -6,6 +6,7 @@ const trackGroup = new THREE.Group();
 
 export function setupTrack() {
   trackGroup.clear();
+  trackGroup.position.set(0, 0, 0);
   createTrackDecorations(trackGroup);
 
   return trackGroup;
