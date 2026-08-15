322018c16a7e64df0eb9b21db43faf24d2edc9d3
Jacek Kopecky <jacek@jacek.cz>
2026-08-15 22:50:12 +0100

    recycle bullets to limit GC

--- src/run/three/run-objects/bullet.ts
+++ src/run/three/run-objects/bullet.ts
@@ -8,13 +8,24 @@ import { createBulletModel, explosionTemplate } from '../models';
 import { getExtentFrontY, markAsDying } from '../resources';
 
 export function createBullet(player: THREE.Object3D): THREE.Object3D {
-  const bullet = createBulletModel();
-  bullet.userData.extent2d = new Circle(undefined, dim.modelSizes.bullet[1] / 2);
-  bullet.userData.type = 'bullet';
-  bullet.translateY((player.userData.gunHeight ?? dim.modelSizes.player[1] / 2) as number);
+  const bullet = createBulletModel(recycledBullets.pop());
 
-  const action = rotateAlways(bullet, dim.bulletRotationsPerSecond, 'z');
-  bullet.addEventListener('removed', () => action.stop());
+  bullet.userData.type = 'bullet';
+  bullet.userData.extent2d ??= new Circle(undefined, dim.modelSizes.bullet[1] / 2);
+
+  const yTranslation = (player.userData.gunHeight ?? dim.modelSizes.player[1] / 2) as number;
+  bullet.userData._yTranslation = yTranslation;
+  bullet.translateY(yTranslation);
+
+  if (!bullet.userData._action) {
+    const action = rotateAlways(bullet, dim.bulletRotationsPerSecond, 'z');
+    bullet.userData._action = action;
+    bullet.addEventListener('removed', () => {
+      action.paused = true;
+    });
+  } else {
+    (bullet.userData._action as THREE.AnimationAction).paused = false;
+  }
 
   return bullet;
 }
@@ -30,5 +41,25 @@ export function killBullet(bullet: THREE.Object3D, objectExtent: THREE.Box2 | Ci
 
   markAsDying(bullet);
 
-  setTimeout(() => bullet.removeFromParent(), dim.playerBulletDyingDuration * 1000);
+  setTimeout(() => {
+    bullet.removeFromParent();
+    recycleBullet(bullet);
+  }, dim.playerBulletDyingDuration * 1000);
 }
+
+export function recycleBullet(bullet: THREE.Object3D) {
+  if (bullet instanceof THREE.Mesh) {
+    markAsDying(bullet, false);
+    recycledBullets.push(bullet as THREE.Mesh);
+    if (typeof bullet.userData._yTranslation === 'number') {
+      bullet.translateY(-bullet.userData._yTranslation);
+      bullet.userData._yTranslation = undefined;
+    }
+  }
+}
+
+export function freeRecycledBullets() {
+  recycledBullets.length = 0;
+}
+
+const recycledBullets: THREE.Mesh[] = [];
